4062c9b7ca89c5f690e827655a6ba4965c5bb15a
Dario Elyasy <dario-e@beyond-eye.com>
2022-10-03 10:23:42 +0300

    changed app title

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Horizontal Picker',
+      title: 'Scrollable Timeline',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.grey,
