e706f84eb08b4fffcd04ebde1f1b3aa19da14815
Dario Elyasy <dario-e@beyond-eye.com>
2022-10-06 10:03:22 +0300

    fixed build.gradle for added youtube lib deps and added example menu screen

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,17 +4,57 @@ import 'basic_example_page.dart';
 
 void main() => runApp(MyApp());
 
+class RouteNames {
+  static const String root="/";
+  static const String simple="/simple";
+  static const String youtube="/youtube";
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Scrollable Timeline',
-      debugShowCheckedModeBanner: false,
+      title: 'Scrollable Timeline Demo',
+      initialRoute: RouteNames.root,
+      routes: {
+        RouteNames.root: (context) => MainScreen(),
+        RouteNames.simple: (context) => BasicExamplePage(),
+        RouteNames.youtube: (context) => BasicExamplePage(),
+      },
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.green,
       ),
-      home: BasicExamplePage(),
     );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  final ButtonStyle btnStyle =
+  ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20),backgroundColor: Colors.green);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("ScrollableTimeline example")),
+        body: Center(
+      child: Column(
+        children: [
+          ElevatedButton(
+              style: btnStyle,
+              onPressed: () {
+                Navigator.pushNamed(context, RouteNames.simple);
+              },
+              child: Text("simple example")),
+          ElevatedButton(
+              style: btnStyle,
+              onPressed: () {
+                Navigator.pushNamed(context, RouteNames.youtube);
+              },
+              child: Text("youtube example")),
+        ],
+      ),
+    ));
   }
 }
