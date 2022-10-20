066285e1c61b179e3245e5863ee74e165dcea426
Dario Elyasy <dario-e@beyond-eye.com>
2022-10-20 09:53:54 +0300

    added youtube_time_ticker.dart

---
+++ example/lib/utils/youtube_time_ticker.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+class YoutubeTimeTicker {
+  final double tmin;
+  final double tmax;
+  final double tstep;
+  final int? ticks;
+  double curt;
+  late Stream<double> stream;
+  // see https://dart.dev/articles/libraries/creating-streams
+  YoutubeTimeTicker(this.tmin, this.tmax, {this.tstep = 1.0, this.ticks=1000}):curt=tmin {
+    final interval =Duration(microseconds: (tstep*1000000).toInt());
+    late StreamController<double> controller;
+    Timer? timer;
+    int counter = 0;
+
+    void tick(_) {
+      curt += tstep;
+      curt = curt.clamp(tmin, tmax);
+      controller.add(curt); // Ask stream to send counter values as event.
+      if (counter == ticks) {
+        timer?.cancel();
+        controller.close(); // Ask stream to shut down and tell listeners.
+      }
+    }
+
+    void startTimer() {
+      timer = Timer.periodic(interval, tick);
+    }
+
+    void stopTimer() {
+      timer?.cancel();
+      timer = null;
+    }
+
+    controller = StreamController<double>.broadcast(
+        onListen: startTimer,
+//        onPause: stopTimer,
+//        onResume: startTimer,
+        onCancel: stopTimer);
+
+    stream= controller.stream;
+  }
+
+}
