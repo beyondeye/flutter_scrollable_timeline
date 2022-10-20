45a35d1c5c63724852f5f8408024e2ffcee4d943
Dario Elyasy <dario-e@beyond-eye.com>
2022-10-20 13:24:17 +0300

    fixes to youtube_time_ticker.dart

--- example/lib/utils/youtube_time_ticker.dart
+++ example/lib/utils/youtube_time_ticker.dart
@@ -4,36 +4,44 @@ import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 /// a class to periodically fetch the current playing time from a YoutubePlayerController and
 /// send the value to broadcast stream
+/// TODO look at implementation of YoutubePlayerController.getCurrentPositionStream
 class YoutubeTimeTicker {
   final YoutubePlayerController yt;
   final double timeFetchDelay;
   double curt;
+  late StreamController<double> _controller;
   late Stream<double> stream;
+  Timer? _timer;
   // see https://dart.dev/articles/libraries/creating-streams
-  YoutubeTimeTicker({required this.yt, required this.timeFetchDelay}):curt=0 {
+
+  Future<void> tick(_) async {
+    curt = await yt.currentTime;
+    _controller.add(curt); // Ask stream to send counter values as event.
+  }
+
+  void _startTimer() {
     final interval =Duration(microseconds: (timeFetchDelay*1000000).toInt());
-    late StreamController<double> controller;
-    Timer? timer;
+    _timer = Timer.periodic(interval, tick);
+  }
 
-    Future<void> tick(_) async {
-      curt = await yt.currentTime;
-      controller.add(curt); // Ask stream to send counter values as event.
-    }
+  void _stopTimer() {
+    _timer?.cancel();
+    _timer = null;
+  }
 
-    void startTimer() {
-      timer = Timer.periodic(interval, tick);
-    }
+  YoutubeTimeTicker({required this.yt, required this.timeFetchDelay}):curt=0 {
 
-    void stopTimer() {
-      timer?.cancel();
-      timer = null;
-    }
 
-    controller = StreamController<double>.broadcast(
-        onListen: startTimer,
-        onCancel: stopTimer);
+    _controller = StreamController<double>.broadcast(
+        onListen: _startTimer,
+        onCancel: _stopTimer);
 
-    stream= controller.stream;
+    stream= _controller.stream;
+  }
+
+  void cancel() {
+    _stopTimer();
+    _controller.close();
   }
 
 }
