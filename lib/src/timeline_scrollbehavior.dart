31a5a2342dc1f5deb4973704f164b4cecb571016
Dario Elyasy <dario-e@beyond-eye.com>
2022-10-23 18:46:03 +0300

    updated doc comments for timeline_scrollbehavior.dart

--- lib/src/timeline_scrollbehavior.dart
+++ lib/src/timeline_scrollbehavior.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-/// [TimelineScrollBehavior]  is needed for making a ScrollView scrollable with mouse
+/// [TimelineScrollBehavior]  is needed for making a ScrollView scrollable with mouse.
 /// see https://docs.flutter.dev/release/breaking-changes/default-scroll-behavior-drag
 class TimelineScrollBehavior extends MaterialScrollBehavior {
   @override
