eb94340cfc2f8a768f5a17766347f42399d450c9
Dario Elyasy <dario-e@beyond-eye.com>
2022-10-24 12:25:35 +0300

    dragging_state_provider.dart: finalized dart doc strings

--- lib/src/dragging_state_provider.dart
+++ lib/src/dragging_state_provider.dart
@@ -4,13 +4,20 @@ abstract class IScrollableTimelineDraggingState {
   abstract bool isDragging;
 }
 
+/// internal class used in [ScrollableTimeline] when its dragging state is
+/// not shared with other timelines (the default situation)
 class NonSharedDraggingState  implements IScrollableTimelineDraggingState {
   NonSharedDraggingState();
   @override
   bool isDragging=false;
 }
 
-// see https://api.flutter.dev/flutter/widgets/InheritedWidget-class.html
+/// a widget that provides a shared [isDragging] state to children
+/// [ScrollableTimeline]
+/// widgets, so that they will all stop reacting to external time streams as soon
+/// as any of them is dragged.
+///
+/// see https://api.flutter.dev/flutter/widgets/InheritedWidget-class.html
 class ScrollableTimelineSharedDragging extends InheritedWidget implements IScrollableTimelineDraggingState {
   ScrollableTimelineSharedDragging({
     super.key,
