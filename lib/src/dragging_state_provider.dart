27ce733028bb87df2ae1a9a67844eb9143107677
Dario Elyasy <dario-e@beyond-eye.com>
2022-10-25 14:43:54 +0300

    dragging_state_provider.dart reformatted with flutter format

--- lib/src/dragging_state_provider.dart
+++ lib/src/dragging_state_provider.dart
@@ -6,10 +6,10 @@ abstract class IScrollableTimelineDraggingState {
 
 /// internal class used in [ScrollableTimeline] when its dragging state is
 /// not shared with other timelines (the default situation)
-class NonSharedDraggingState  implements IScrollableTimelineDraggingState {
+class NonSharedDraggingState implements IScrollableTimelineDraggingState {
   NonSharedDraggingState();
   @override
-  bool isDragging=false;
+  bool isDragging = false;
 }
 
 /// a widget that provides a shared [isDragging] state to children
@@ -18,22 +18,24 @@ class NonSharedDraggingState  implements IScrollableTimelineDraggingState {
 /// as any of them is dragged.
 ///
 /// see https://api.flutter.dev/flutter/widgets/InheritedWidget-class.html
-class ScrollableTimelineSharedDragging extends InheritedWidget implements IScrollableTimelineDraggingState {
+class ScrollableTimelineSharedDragging extends InheritedWidget
+    implements IScrollableTimelineDraggingState {
   ScrollableTimelineSharedDragging({
     super.key,
     required super.child,
   });
 
   @override
-  bool isDragging= false;
+  bool isDragging = false;
 
   static ScrollableTimelineSharedDragging? of(BuildContext context) {
-    final ScrollableTimelineSharedDragging? result = context.dependOnInheritedWidgetOfExactType<ScrollableTimelineSharedDragging>();
+    final ScrollableTimelineSharedDragging? result = context
+        .dependOnInheritedWidgetOfExactType<ScrollableTimelineSharedDragging>();
 //    assert(result != null, 'No ScrollableTimelineDraggingState found in context');
     return result;
   }
+
   @override
-  bool updateShouldNotify(ScrollableTimelineSharedDragging old) => isDragging != old.isDragging;
-
-
+  bool updateShouldNotify(ScrollableTimelineSharedDragging old) =>
+      isDragging != old.isDragging;
 }
